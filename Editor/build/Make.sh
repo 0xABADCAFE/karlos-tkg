55610b0d669477026a96cbc3bc4f18529bfb64f0
0xABADCAFE <karlchurchill@gmail.com>
2025-12-07 13:28:53 +0000

    Update paths

--- Editor/build/Make.sh
+++ Editor/build/Make.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-./MakeModProperties -i ../ActiveData/ModProperties/game.props.json -o ../../Game/HighSpec/Includes/game.props
-./MakeModProperties -i ../ActiveData/ModProperties/game.props.low.json -o ../../Game/LowSpec/Includes/game.props
+./MakeModProperties -i ../Assets/ModProperties/game.props.json -o ../../Game/HighSpec/Includes/game.props
+./MakeModProperties -i ../Assets/ModProperties/game.props.low.json -o ../../Game/LowSpec/Includes/game.props
