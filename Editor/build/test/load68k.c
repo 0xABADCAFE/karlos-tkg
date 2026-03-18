eca6e1606c05197f1dee577b1dfd60bd20ddff78
0xABADCAFE <karlchurchill@gmail.com>
2026-03-18 23:07:28 +0000

    Achievements parser.

--- Editor/build/test/load68k.c
+++ Editor/build/test/load68k.c
@@ -26,72 +26,115 @@ enum {
 };
 
 /**
- * DataOffset type. This can be converted to an actual address after loading.
+ * GMF_DataOffset
+ *
+ * Represents an offset in the file that is to be resolved to an actual address during parsing.
  */
 typedef union {
     ULONG       do_Offset;
     char const* do_Text;
     UBYTE*      do_ByteAddress;
-} ALIGN(sizeof(ULONG)) DataOffset;
+} ALIGN(sizeof(ULONG)) GMF_DataOffset;
 
 /**
- * Basic Version tuple for checks
+ * GMF_Version
+ *
+ * Basic major.minor version tuple used for version checks.
  */
 typedef struct {
     UWORD       v_Major;
     UWORD       v_Minor;
-} ALIGN(sizeof(ULONG)) Version;
+} ALIGN(sizeof(ULONG)) GMF_Version;
 
+/**
+ * GMF_Ident
+ *
+ * Basic 4-byte ident. Accessible either as a 32-bit word or as 4 characters. Big endian layout.
+ */
 typedef union {
     char        id_Text[4];
     ULONG       id_Value;
-} ALIGN(sizeof(ULONG)) Ident;
+} ALIGN(sizeof(ULONG)) GMF_Ident;
 
+/**
+ * GMF_Header
+ *
+ * Main game modification file header.
+ */
 typedef struct {
-    Ident       h_Ident;
-    Ident       h_SubFormat;
-    Version     h_RequiresVersion;
-    Version     h_Version;
-    DataOffset  h_Description;
-} ALIGN(sizeof(ULONG)) Header;
+    GMF_Ident       h_Ident;
+    GMF_Ident       h_SubFormat;
+    GMF_Version     h_RequiresVersion;
+    GMF_Version     h_Version;
+    GMF_DataOffset  h_Description;
+} ALIGN(sizeof(ULONG)) GMF_Header;
 
+/**
+ * GMF_ChunkHeader
+ *
+ * Minimalist header for each Chunk.
+ */
 typedef struct {
-    Ident       ch_Ident;
+    GMF_Ident   ch_Ident;
     ULONG       ch_Length;
-} ALIGN(sizeof(ULONG)) ChunkHeader;
+} ALIGN(sizeof(ULONG)) GMF_ChunkHeader;
 
+/**
+ * GMF_IndexEntry
+ *
+ * Basic Ident/Offset pair for the common Index chunk.
+ */
 typedef struct {
-    Ident       ie_Ident;
-    DataOffset  ie_Offset;
-} ALIGN(sizeof(ULONG)) IndexEntry;
+    GMF_Ident       ie_Ident;
+    GMF_DataOffset  ie_Offset;
+} ALIGN(sizeof(ULONG)) GMF_IndexEntry;
 
+/**
+ * GMF_Data
+ *
+ * Main structure for loaded Game Modification Files.
+ */
 typedef struct {
-    UBYTE*            gmd_Data;
-    ULONG             gmd_Length;
-    ULONG             gmd_IndexSize;
-    Header const*     gmd_Header;
-    IndexEntry const* gmd_Index;
-    char const*       gmd_Strings;
-} ALIGN(sizeof(ULONG)) GMFData;
+    UBYTE*                gmd_Data;
+    ULONG                 gmd_Length;
+    ULONG                 gmd_IndexSize;
+    GMF_Header const*     gmd_Header;
+    GMF_IndexEntry const* gmd_Index;
+    char const*           gmd_Strings;
+} ALIGN(sizeof(ULONG)) GMF_Data;
 
 
 /**
- * Custom parsers
+ * GMF_ChunkParser
+ *
+ * Callable type for parsing chunks after loading.
  */
-typedef BOOL (*ChunkParser)(ChunkHeader const* pChunkHeader, GMFData* pGMFData);
+typedef BOOL (*GMF_ChunkParser)(GMF_ChunkHeader const* pChunkHeader, GMF_Data* pGMFData);
 
+/**
+ * GMF_ParserEntry
+ *
+ * Basic Ident/Parser pair for associating a parser to a particular chunk type.
+ */
 typedef struct {
     ULONG pe_Ident;
-    ChunkParser pe_Parser;
-} ALIGN(sizeof(ULONG)) ParserEntry;
+    GMF_ChunkParser pe_Parser;
+} ALIGN(sizeof(ULONG)) GMF_ParserEntry;
 
-static BOOL gmf_CheckData(GMFData* pGMFData, Header const* pAgainst) {
+/**********************************************************************************************************************/
+
+/**
+ * gmf_CheckData()
+ *
+ * Basic valiation for Game Modification Files based on expected header properties.
+ */
+static BOOL gmf_CheckData(GMF_Data* pGMFData, GMF_Header const* pAgainst) {
     puts("\tgmf_CheckData()");
 
     if (!pGMFData || !pAgainst || !pGMFData->gmd_Data) {
         return FALSE;
     }
-    Header const* pFrom = (Header const*)pGMFData->gmd_Data;
+    GMF_Header const* pFrom = (GMF_Header const*)pGMFData->gmd_Data;
 
     return
         pAgainst->h_Ident.id_Value     == pFrom->h_Ident.id_Value &&
@@ -103,10 +146,12 @@ static BOOL gmf_CheckData(GMFData* pGMFData, Header const* pAgainst) {
 
 
 /**
- * Attempts to load the named file, populating the data and length fields of
- * the GMFData and zeroing the rest. Once loaded, the data must be validated.
+ * gmf_ReadFile()
+ *
+ * Attempts to load the named file, populating the data and length fields of the GMF_Data and zeroing the rest.
+ * Once loaded, the data must be validated.
  */
-static BOOL gmf_ReadFile(char const* filename, GMFData* pGMFData)
+static BOOL gmf_ReadFile(char const* filename, GMF_Data* pGMFData)
 {
     puts("\tgmf_ReadFile()");
 
@@ -169,19 +214,25 @@ static BOOL gmf_ReadFile(char const* filename, GMFData* pGMFData)
     return bResult;
 }
 
-static BOOL gmf_ProcessDefaultChunks(GMFData* pGMFData)
+/**
+ * gmf_ProcessDefaultChunks()
+ *
+ * Handles parsing the default chunks common to all Game Modification Format files, i.e. the Index and String
+ * chunks.
+ */
+static BOOL gmf_ProcessDefaultChunks(GMF_Data* pGMFData)
 {
     puts("\tgmf_ProcessDefaultChunks()");
-    ChunkHeader const* pIndexHeader = (ChunkHeader const*)(pGMFData->gmd_Data + sizeof(Header));
+    GMF_ChunkHeader const* pIndexHeader = (GMF_ChunkHeader const*)(pGMFData->gmd_Data + sizeof(GMF_Header));
     if (
         pIndexHeader->ch_Ident.id_Value != IDENT_INDX ||
-        pIndexHeader->ch_Length < (sizeof(ChunkHeader) + sizeof(IndexEntry))
+        pIndexHeader->ch_Length < (sizeof(GMF_ChunkHeader) + sizeof(GMF_IndexEntry))
     ) {
         return FALSE;
     }
-    int iNumEntries = (pIndexHeader->ch_Length - sizeof(ChunkHeader))/sizeof(IndexEntry);
+    int iNumEntries = (pIndexHeader->ch_Length - sizeof(GMF_ChunkHeader))/sizeof(GMF_IndexEntry);
 
-    IndexEntry* pIndexEntry = (IndexEntry*)(((UBYTE*)pIndexHeader) + sizeof(ChunkHeader));
+    GMF_IndexEntry* pIndexEntry = (GMF_IndexEntry*)(((UBYTE*)pIndexHeader) + sizeof(GMF_ChunkHeader));
     pGMFData->gmd_IndexSize  = iNumEntries;
     pGMFData->gmd_Index      = pIndexEntry;
 
@@ -194,7 +245,7 @@ static BOOL gmf_ProcessDefaultChunks(GMFData* pGMFData)
         pIndexEntry[i].ie_Offset.do_ByteAddress = pGMFData->gmd_Data + pIndexEntry[i].ie_Offset.do_Offset;
 
         /* Quick sanity check - ensure the index ident is a match for the in memory location */
-        Ident const* pChunkIdent = (Ident const*)pIndexEntry[i].ie_Offset.do_ByteAddress;
+        GMF_Ident const* pChunkIdent = (GMF_Ident const*)pIndexEntry[i].ie_Offset.do_ByteAddress;
         if (pIndexEntry[i].ie_Ident.id_Value != pChunkIdent->id_Value) {
             printf(
                 "Index entry %d ident mismatch. Index: %.*s => Chunk: %.*s\n",
@@ -216,40 +267,63 @@ static BOOL gmf_ProcessDefaultChunks(GMFData* pGMFData)
     }
 
     /* Patch the description locaton */
-    Header* pHeader = (Header*)pGMFData->gmd_Data;
+    GMF_Header* pHeader = (GMF_Header*)pGMFData->gmd_Data;
     pHeader->h_Description.do_Text = pGMFData->gmd_Strings + pHeader->h_Description.do_Offset;
     pGMFData->gmd_Header = pHeader;
     return TRUE;
 }
 
-static void* gmf_ChunkData(ChunkHeader const * pHeader)
+/**
+ * gmf_ChunkData()
+ *
+ * Returns the start of the chunk body data for a given header. This is simply the data immediately following.
+ * Assumes the returned data requires modification.
+ */
+static void* gmf_ChunkData(GMF_ChunkHeader const *pHeader)
 {
-    return ((UBYTE*)pHeader) + sizeof(ChunkHeader);
+    return ((UBYTE*)pHeader) + sizeof(GMF_ChunkHeader);
 }
 
-static const char* gmf_RelocateString(char const* string, GMFData const* pGMFData)
+/**
+ * gmf_ResolveString()
+ *
+ * For a given initial string reference, resolves an offset to the actual in-memory location within the common
+ * string chunk.
+ */
+static const char* gmf_ResolveString(char const* string, GMF_Data const* pGMFData)
 {
     return (string < pGMFData->gmd_Strings) ? pGMFData->gmd_Strings + (size_t)string : string;
 }
 
-ChunkHeader const* GMF_LocateChunk(GMFData const* pGMFData, ULONG iIdentValue)
+/**
+ * GMF_LocateChunk()
+ *
+ * Returns the first encountered instance of a chunk with the supplied index. It is assumed that a game modification
+ * file only contains one of each chunk type.
+ */
+GMF_ChunkHeader const* GMF_LocateChunk(GMF_Data const* pGMFData, ULONG iIdentValue)
 {
     for (int i = 0; i < pGMFData->gmd_IndexSize; ++i) {
         if (pGMFData->gmd_Index[i].ie_Ident.id_Value == iIdentValue) {
-            return (ChunkHeader const *)pGMFData->gmd_Index[i].ie_Offset.do_ByteAddress;
+            return (GMF_ChunkHeader const *)pGMFData->gmd_Index[i].ie_Offset.do_ByteAddress;
         }
     }
     return NULL;
 }
 
-
-GMFData* GMF_LoadFile(char const* filename, Header const* pCheckHeader, ParserEntry const* pCustomParsers)
+/**
+ * GMF_LoadFile()
+ *
+ * Attempts to load the specified Game Modification File and process with a null terminated list of user supplied
+ * parsers for any custom chunk types that are present.
+ */
+GMF_Data* GMF_LoadFile(char const* filename, GMF_Header const* pCheckHeader, GMF_ParserEntry const* pCustomParsers)
 {
     puts("GMF_LoadFile()");
     if (!filename || !pCheckHeader) {
         return NULL;
     }
-    GMFData* pGMFData = (GMFData*)calloc(1, sizeof(GMFData));
+    GMF_Data* pGMFData = (GMF_Data*)calloc(1, sizeof(GMF_Data));
     if (!pGMFData) {
         return NULL;
     }
@@ -263,7 +337,7 @@ GMFData* GMF_LoadFile(char const* filename, Header const* pCheckHeader, ParserEn
     }
 
     if (pCustomParsers) {
-        ChunkHeader const* pChunkHeader = NULL;
+        GMF_ChunkHeader const* pChunkHeader = NULL;
         while (
             pCustomParsers->pe_Ident &&
             pCustomParsers->pe_Parser
@@ -278,7 +352,12 @@ GMFData* GMF_LoadFile(char const* filename, Header const* pCheckHeader, ParserEn
     return pGMFData;
 }
 
-void GMF_Free(GMFData* pGMFData)
+/**
+ * GMF_Free()
+ *
+ * Releases a GMF_Data instance and any associated data.
+ */
+void GMF_Free(GMF_Data* pGMFData)
 {
     puts("GMF_Free()");
     if (pGMFData) {
@@ -291,8 +370,10 @@ void GMF_Free(GMFData* pGMFData)
 
 /**********************************************************************************************************************/
 
-
-Header const reference = {
+/**
+ * Main Game Modification File, GMOD
+ */
+GMF_Header const reference = {
     .h_Ident.id_Value     = IDENT_TKGD,
     .h_SubFormat.id_Value = IDENT_GMOD,
     .h_Version            = {1, 255}
@@ -305,22 +386,49 @@ enum {
     IDENT_ACHV = 0x41434856,
 };
 
+/**
+ * GMod_Reward
+ *
+ * Defines a reward structure, which applied modifications to player items and carry limits.
+ */
 typedef struct {
     char const* r_Description;
-    UWORD r_CarryOffset;
-    UWORD r_ImmediateOffset;
-    UWORD r_RewardData[1];
-} ALIGN(sizeof(ULONG)) Reward;
+    UWORD       r_CarryOffset;
+    UWORD       r_ImmediateOffset;
+    UWORD       r_RewardData[1];
+} ALIGN(sizeof(ULONG)) GMod_Reward;
 
+/**
+ * GMod_SpecialAmmoBonus
+ *
+ * Defines rewards associated with collecting of special ammo types.
+ */
 typedef struct {
-    UWORD         spab_Index;
-    UWORD         spab_AmmoID;
-    Reward const* spab_Reward;
-} ALIGN(sizeof(ULONG)) SpecialAmmoBonus;
+    UWORD              spab_Index;
+    UWORD              spab_AmmoID;
+    GMod_Reward const* spab_Reward;
+} ALIGN(sizeof(ULONG)) GMod_SpecialAmmoBonus;
+
+/**
+ * GMod_Achievement
+ *
+ * Defines rewards associated with collecting of special ammo types.
+ */
+typedef struct {
+    char const*        achv_Description;
+    GMod_Reward const* achv_Reward;
+    UWORD              achv_RuleType;
+    UWORD              achv_Params[3];
+} ALIGN(sizeof(ULONG)) GMod_Achievement;
 
 /**********************************************************************************************************************/
 
-BOOL gmod_ParseDummy(ChunkHeader const* pChunkHeader, GMFData* pGMFData)
+/**
+ * gmod_ParseDummy()
+ *
+ * Dummy chunk parser just for evaluation purposes.
+ */
+BOOL gmod_ParseDummy(GMF_ChunkHeader const* pChunkHeader, GMF_Data* pGMFData)
 {
     printf(
         "\tgmod_ParseDummy() %.*s\n",
@@ -329,30 +437,41 @@ BOOL gmod_ParseDummy(ChunkHeader const* pChunkHeader, GMFData* pGMFData)
     return TRUE;
 }
 
-static Reward* gmod_RelocateReward(Reward const* pReward, ChunkHeader const* pRewardChunk)
+/**
+ * gmod_ResolveReward()
+ *
+ * For a given initial reward reference, resolves an offset to the actual in memory location of the GMod_Reward
+ * instance.
+ */
+static GMod_Reward* gmod_ResolveReward(GMod_Reward const* pReward, GMF_ChunkHeader const* pRewardChunk)
 {
-    return (Reward*)((UBYTE*)pReward + (size_t)pRewardChunk);
+    return (GMod_Reward*)((UBYTE*)pReward + (size_t)pRewardChunk);
 }
 
-BOOL gmod_ParseSpecialAmmoBonuses(ChunkHeader const* pChunkHeader, GMFData* pGMFData)
+/**
+ * gmod_ParseSpecialAmmoBonuses()
+ *
+ * Parser for the Special Ammo Bonuses Chunk
+ */
+BOOL gmod_ParseSpecialAmmoBonuses(GMF_ChunkHeader const* pChunkHeader, GMF_Data* pGMFData)
 {
     printf(
         "\tgmod_ParseSpecialAmmoBonuses() %.*s\n",
         4, pChunkHeader->ch_Ident.id_Text
     );
-    ChunkHeader const* pRewardChunk = GMF_LocateChunk(pGMFData, IDENT_RWRD);
-    SpecialAmmoBonus* pSPAB = (SpecialAmmoBonus*)gmf_ChunkData(pChunkHeader);
+    GMF_ChunkHeader const* pRewardChunk = GMF_LocateChunk(pGMFData, IDENT_RWRD);
+    GMod_SpecialAmmoBonus* pSPAB = (GMod_SpecialAmmoBonus*)gmf_ChunkData(pChunkHeader);
     while (pSPAB->spab_Index != 0xFFFF) {
         if (pSPAB->spab_Reward > 0) {
             printf(
-                "\t\tRelocating SPAB %d [%d] Reward [%p + %zu] => ",
+                "\t\tResolving SPAB %d [%d]\n\t\t\tReward [%p + %zu] ",
                 (int)pSPAB->spab_Index,
                 (int)pSPAB->spab_AmmoID,
                 pRewardChunk,
                 (size_t)pSPAB->spab_Reward
             );
-            Reward* pReward        = gmod_RelocateReward(pSPAB->spab_Reward, pRewardChunk);
-            pReward->r_Description = gmf_RelocateString(pReward->r_Description, pGMFData);
+            GMod_Reward* pReward   = gmod_ResolveReward(pSPAB->spab_Reward, pRewardChunk);
+            pReward->r_Description = gmf_ResolveString(pReward->r_Description, pGMFData);
             pSPAB->spab_Reward     = pReward;
             puts(pReward->r_Description);
         }
@@ -361,30 +480,69 @@ BOOL gmod_ParseSpecialAmmoBonuses(ChunkHeader const* pChunkHeader, GMFData* pGMF
     return TRUE;
 }
 
-BOOL gmod_ParseAchievements(ChunkHeader const* pChunkHeader, GMFData* pGMFData)
+/**
+ * gmod_ParseAchievements()
+ *
+ * Parser for the Special Ammo Bonuses Chunk
+ */
+BOOL gmod_ParseAchievements(GMF_ChunkHeader const* pChunkHeader, GMF_Data* pGMFData)
 {
     printf(
         "\tgmod_ParseAchievements() %.*s\n",
         4, pChunkHeader->ch_Ident.id_Text
     );
-    ChunkHeader const* pRewardChunk = GMF_LocateChunk(pGMFData, IDENT_RWRD);
+    GMF_ChunkHeader const* pRewardChunk = GMF_LocateChunk(pGMFData, IDENT_RWRD);
+    GMod_Achievement*      pAchievement = (GMod_Achievement*)gmf_ChunkData(pChunkHeader);
+
+    int iNumEntries = pChunkHeader->ch_Length / sizeof(GMod_Achievement);
+
+    for (int i = 0; i < iNumEntries; ++i) {
+        pAchievement->achv_Description = gmf_ResolveString(pAchievement->achv_Description, pGMFData);
+        if (pAchievement->achv_Reward > 0) {
+            printf(
+                "\t\tResolving ACHV %d [Rule %d] [Name %s]\n\t\t\tReward [%p + %zu] ",
+                i,
+                (int)pAchievement->achv_RuleType,
+                pAchievement->achv_Description,
+                pRewardChunk,
+                (size_t)pAchievement->achv_Reward
+            );
+            GMod_Reward* pReward   = gmod_ResolveReward(pAchievement->achv_Reward, pRewardChunk);
+            pReward->r_Description = gmf_ResolveString(pReward->r_Description, pGMFData);
+            pAchievement->achv_Reward = pReward;
+            puts(pReward->r_Description);
+        } else {
+            printf(
+                "\t\tResolving ACHV %d [Rule %d] [Name %s]\n",
+                i,
+                (int)pAchievement->achv_RuleType,
+                pAchievement->achv_Description
+            );
+        }
+        ++pAchievement;
+    }
 
     return TRUE;
 }
 
+/**********************************************************************************************************************/
+
 /**
  * Zero terminated list of custom parser functions for specific idents
  */
-ParserEntry parsers[] = {
+GMF_ParserEntry parsers[] = {
     { IDENT_INVL, gmod_ParseDummy },
     { IDENT_SPAB, gmod_ParseSpecialAmmoBonuses },
     { IDENT_ACHV, gmod_ParseAchievements },
     { 0, NULL },
 };
 
+/**
+ * Test it out...
+ */
 int main(void) {
 
-    GMFData* pGMFData = GMF_LoadFile("mods/redux.props", &reference, parsers);
+    GMF_Data* pGMFData = GMF_LoadFile("mods/redux.props", &reference, parsers);
     if (pGMFData) {
         printf(
             "\nInitial load successful!\n"
@@ -399,7 +557,7 @@ int main(void) {
             (int)pGMFData->gmd_Header->h_RequiresVersion.v_Minor
         );
         for (int i = 0; i < pGMFData->gmd_IndexSize; ++i) {
-            ChunkHeader const *pChunkHeader = (ChunkHeader const *)pGMFData->gmd_Index[i].ie_Offset.do_ByteAddress;
+            GMF_ChunkHeader const *pChunkHeader = (GMF_ChunkHeader const *)pGMFData->gmd_Index[i].ie_Offset.do_ByteAddress;
             printf(
                 "\t%d %.*s : %p %u\n",
                 i,
@@ -411,5 +569,5 @@ int main(void) {
         GMF_Free(pGMFData);
     }
 
-	return 0;
+    return 0;
 }
