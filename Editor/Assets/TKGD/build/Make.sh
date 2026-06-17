70b9104813b241cba3be3c844ae02747cfb157e0
0xABADCAFE <karlchurchill@gmail.com>
2026-06-17 23:36:12 +0100

    Porting errata data.

--- Editor/Assets/TKGD/build/Make.sh
+++ Editor/Assets/TKGD/build/Make.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 ./MakeGameProperties -b ../Source -f redux_high.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_A_high.rson -g ../../../../Game
+./MakeLevelProperties -b ../Source/ -f level_H.rson -g ../../../../Game
