c81176ba7068a76c6ee1d0ffae5d463c3630d1b8
0xABADCAFE <karlchurchill@gmail.com>
2026-06-21 15:14:37 +0100

    Sorting out assets and cruft.

--- Editor/Assets/TKGD/build/Make.sh
+++ Editor/Assets/TKGD/build/Make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 ./MakeGameProperties -b ../Source -f redux_high.rson -g ../../../../Game
+./MakeGameProperties -b ../Source -f redux_low.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_A_high.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_B.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_H.rson -g ../../../../Game
