67903e6d7d1546ae4214c3236d777a4ca88608a6
0xABADCAFE <karlchurchill@gmail.com>
2026-06-21 15:01:06 +0100

    Updated engine to 1.2, added new TKGD files for levels A, B and H

--- Editor/Assets/TKGD/build/Make.sh
+++ Editor/Assets/TKGD/build/Make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 ./MakeGameProperties -b ../Source -f redux_high.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_A_high.rson -g ../../../../Game
+./MakeLevelProperties -b ../Source/ -f level_B.rson -g ../../../../Game
 ./MakeLevelProperties -b ../Source/ -f level_H.rson -g ../../../../Game
